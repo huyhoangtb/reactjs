48cd86fbcbab1b176a0ace9769f09805bab601d5
Peter Hoang Nguyen <vntopmas@gmail.com>
2017-03-31 10:14:26 +0700

    add sagas

--- webpack.config.js
+++ webpack.config.js
@@ -73,19 +73,19 @@ module.exports = {
         }),
         new webpack.HotModuleReplacementPlugin(),
         // removes a lot of debugging code in React
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        }),
-        // keeps hashes consistent between compilations
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        // minifies your code
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                warnings: false
-            }
-        })
+        // new webpack.DefinePlugin({
+        //     'process.env': {
+        //         'NODE_ENV': JSON.stringify('production')
+        //     }
+        // }),
+        // // keeps hashes consistent between compilations
+        // new webpack.optimize.OccurrenceOrderPlugin(),
+        // // minifies your code
+        // new webpack.optimize.UglifyJsPlugin({
+        //     compressor: {
+        //         warnings: false
+        //     }
+        // })
 
     ],
     stats: {
